53d4e1b61aa4b27511ab29d3e4d8e7c1526dbcd5
redmac135 <ethanzhao1981@gmail.com>
2024-01-27 11:30:54 -0500

    create entries get api endpoint

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import {type Entry} from "@/lib/schema";
 import styles from "./page.module.css";
-import prisma from "@/lib/prisma";
+import { CldVideoPlayer, CldImage } from "next-cloudinary";
+import axios from "axios";
 
 export default function Gallery() {
     const [loading, setLoading] = useState(true);
@@ -14,19 +14,35 @@ export default function Gallery() {
         // add axios call here
         setLoading(false);
         setEntries([]);
+
+        axios.get("/api/entries").then((res) => {
+            setEntries(res.data);
+        })
     }, [])
 
     return (
         <main>
             <h1 className={styles.title}>ECHO</h1>
+            <h5 className={styles.description}>Welcome to your harbour...</h5>
             <section className={styles.galleryWrapper}>
                 <div className={styles.gallery}>
-                    {entries.map((entry) => (
-                        <div key={entry.id} className={styles.galleryItem}>
-                            <img src={entry.image} alt={entry.caption} className={styles.galleryImg} width={280} height={400} />
-                            <p className={styles.galleryItemInfo}>{entry.caption}</p>
-                        </div>
-                    ))}
+                    {entries.map((entry) => {
+                        if (entry.isVideo) {
+                            return (
+                                <div key={entry.id} className={styles.galleryItem}>
+                                    <CldVideoPlayer src={entry.image} width={280} height={400} />
+                                    <p className={styles.galleryItemInfo}>{entry.caption}</p>
+                                </div>
+                            )
+                        } else {
+                            return (
+                                <div key={entry.id} className={styles.galleryItem}>
+                                    <CldImage src={entry.image} alt={entry.caption} className={styles.galleryImg} width={280} height={400} />
+                                    <p className={styles.galleryItemInfo}>{entry.caption}</p>
+                                </div>
+                            )
+                        }
+                    })}
                 </div>
             </section>
         </main>
