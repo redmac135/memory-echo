5b8acf84f11e976a31f933c904d9c6c2f0a7ecf3
redmac135 <ethanzhao1981@gmail.com>
2024-01-27 10:58:03 -0500

    correct image styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
-import Image from 'next';
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section className={styles.container}>
-        
+      <section className={styles.section}>
+        <Image src="/everything.webp" fill objectFit="cover" alt="introductory image" />
       </section>
     </main>
   );
